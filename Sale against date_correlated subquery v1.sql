9ef24eb379c728c680b7669a7ce5030ca6dd0845
szebart <94327639+szebart@users.noreply.github.com>
2021-11-22 18:37:23 +0100

    Update Sale against date_correlated subquery v1.sql

--- Sale against date_correlated subquery v1.sql
+++ Sale against date_correlated subquery v1.sql
@@ -1,23 +1,23 @@
--- wynikiem jest co siê sprzeda³o przed 01-09-2021 a nie sprzeda³o po tej dacie
+-- wynikiem jest co siÃª sprzedaÂ³o przed 01-09-2021 a nie sprzedaÂ³o po tej dacie
 SELECT  
 [Item No_],
 (			
 SELECT 
 CASE WHEN (SELECT CAST(COUNT([Item No_])AS INT)) >0  THEN 'TRUE' ELSE 'FALSE' END AS 'warunek'
-			FROM [dbo].[SalesSTLines_BI] sub
+			FROM [dbo].[SSTLines_BI] sub
 			WHERE main.[Item No_] = sub.[Item No_]
 			AND [Date] >= '2021-09-01'
 			AND [Store No_] IN ('ZAR')
 ) AS 'soldafter9'
 
-FROM [dbo].[SalesSTLines_BI] main
+FROM [dbo].[SSTLines_BI] main
 WHERE [Store No_] IN ('ZAR')
 	AND [Date] < '2021-09-01'
 			
 GROUP BY [Item No_]
 
 
--- wynikiem jest co siê sprzeda³o przed 01-09-2021 a nie sprzeda³o po tej dacie
+-- wynikiem jest co siÃª sprzedaÂ³o przed 01-09-2021 a nie sprzedaÂ³o po tej dacie
 
 
 --SELECT  
@@ -25,13 +25,13 @@ GROUP BY [Item No_]
 --(			
 --SELECT 
 --CASE WHEN EXISTS (SELECT [Item No_]  THEN 'TRUE' ELSE 'FALSE' END AS 'warunek'
---			FROM [dbo].[SalesSTLines_BI] sub
+--			FROM [dbo].[SSTLines_BI] sub
 --			WHERE main.[Item No_] = sub.[Item No_]
 --			AND [Date] >= '2021-09-01'
 --			AND [Store No_] IN ('ZAR')
 --) AS 'soldafter9'
 
---FROM [dbo].[SalesSTLines_BI] main
+--FROM [dbo].[SSTLines_BI] main
 --WHERE [Store No_] IN ('ZAR')
 --	AND [Date] < '2021-09-01'
 			
