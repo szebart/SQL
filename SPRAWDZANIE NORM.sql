c83d1371b4fd2205390e302b88cb4aca2d69fbfd
szebart <94327639+szebart@users.noreply.github.com>
2021-11-22 18:35:09 +0100

    Update SPRAWDZANIE NORM.sql

--- SPRAWDZANIE NORM.sql
+++ SPRAWDZANIE NORM.sql
@@ -1,5 +1,5 @@
 DECLARE @INDEKS AS varchar (15)
-SET @INDEKS = '50065-TK-50' -- TUTAJ WPISZ INDEKS--;
+SET @INDEKS = '565-T0K-50' -- TUTAJ WPISZ INDEKS--;
 
 
 
@@ -13,16 +13,16 @@ WHEN [Field No_] = 11 THEN 'Zmieniona norma MAX'
 WHEN [Field No_] = 12 THEN 'Zmieniona norma MIN'
 WHEN [Field No_] = 17 THEN 'Zmieniona War-Logistyczna'
 WHEN [Field No_] = 15 THEN 'Aktywacja/Deaktywacja'
-ELSE 'Norma usuniêta' END AS [Typ zapisu]
+ELSE 'Norma usuniÃªta' END AS [Typ zapisu]
 ,CASE 
-WHEN [Type of Change] = 2 THEN 'USUNIÊCIE' ELSE 'ZMIANA' END AS [Typ zmiany]
+WHEN [Type of Change] = 2 THEN 'USUNIÃŠCIE' ELSE 'ZMIANA' END AS [Typ zmiany]
 
 ,CASE 
 WHEN [Old Value] = 'true' THEN 'Deaktywacja normy' 
-WHEN [Old Value] = 'false' THEN 'Aktywacja normy' ELSE [Old Value] END AS [stara wartoœæ]
+WHEN [Old Value] = 'false' THEN 'Aktywacja normy' ELSE [Old Value] END AS [stara wartoÅ“Ã¦]
 ,CASE
 WHEN [New Value] = 'false' THEN 'Aktywacja normy' 
-WHEN [New Value] = 'true' THEN 'Deaktywacja normy' ELSE [New Value] END AS [nowa wartoœæ]
+WHEN [New Value] = 'true' THEN 'Deaktywacja normy' ELSE [New Value] END AS [nowa wartoÅ“Ã¦]
 
 
 --,[Old Value] AS [Poprzednia norma/war-log] -- false to aktywacja, true to deaktywacja
@@ -32,12 +32,12 @@ WHEN [New Value] = 'true' THEN 'Deaktywacja normy' ELSE [New Value] END AS [nowa
 ,[Primary Key Field 3 Value] AS [Salon]
 
 	FROM
-		[navsql2].[Kazar].[dbo].[KAZAR$Change Log Entry] WITH (NOLOCK)
-		WHERE [Table No_] = '10012206' AND [Primary Key Field 1 Value] = @INDEKS AND [Field No_] NOT IN (1,2,3,5,9,21,23,26,27,28,29,30,31,50001)
+		[sql2].[Kr].[dbo].[KR$Change Log Entry] WITH (NOLOCK)
+		WHERE [Table No_] = '100122006' AND [Primary Key Field 1 Value] = @INDEKS AND [Field No_] NOT IN (1,2,3,5,9,21,23,26,27,28,29,30,31,50001)
 
-		--ewentualnie mo¿na sobie wyszukaæ po dacie/godzinie lub u¿ytkowniku, lub salonie doklejaj¹c poni¿sze linijki do wiersza powy¿ej:
+		--ewentualnie moÂ¿na sobie wyszukaÃ¦ po dacie/godzinie lub uÂ¿ytkowniku, lub salonie doklejajÂ¹c poniÂ¿sze linijki do wiersza powyÂ¿ej:
 		--AND [Date and Time] > '2020-06-25 00:00:00.000' 
-		--AND [User ID] = 'KAZARFOOTWEAR\BSZETERLAK' 
+		--AND [User ID] = 'KR\xxx' 
 		--AND [Primary Key Field 3 Value] = 'ZWO'
 
 		ORDER BY [Date and Time] DESC
